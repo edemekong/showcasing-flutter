8a14c1c2b3ea6c930f86038f759590414fa6c0f3
Paul Edemekong <hello.paulvin@gmail.com>
2022-01-24 12:32:30 +0100

    showcase textfield controller

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    _controller.addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    print('rebuild this many times');
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: TextField(
+              controller: _controller,
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                if (_controller.text.contains('@')) {
+                  _controller.text = _controller.text;
+                } else {
+                  _controller.text = 'Invalid Email';
+                }
+              });
+            },
+            child: const Text('Press me'),
+          ),
+          Text(_controller.text)
+        ],
+      ),
+    );
   }
 }
